261b80875b79e18edb9a19b14d56f65c0bf58b95
Saad <aleemsaadullah@gmail.com>
2024-09-13 17:08:28 -0400

    convert widget form into a reusable component

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,23 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    'process.env': {
+      NODE_ENV: "production"
+    }
+  },
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    lib: {
+      entry: "./src/index.jsx",
+      name: "widget",
+      fileName: (format) => `widget.${format}.js`,
+    },
+    target: "esnext"
+  }
 })
